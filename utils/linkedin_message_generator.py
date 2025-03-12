063e70acafe789f7f88064eced626d3e86b17045
Praneeth Ravuri <pravdevrav@gmail.com>
2025-03-12 09:57:15 -0500

    Update linkedin_message_generator.py

--- utils/linkedin_message_generator.py
+++ utils/linkedin_message_generator.py
@@ -11,10 +11,10 @@ def generate_linkedin_message(company: str, job_title: str, job_id: str = "", re
     Returns:
         str: The generated LinkedIn message.
     """
-    job_id_text = f" (Job Id: {job_id})" if job_id else ""
+    job_id_text = f"(Job Id: {job_id})" if job_id else ""
     message = (
         f"Hi {recipient_name}, I'm a Software Engineer at Prodapt in Dallas exploring new opportunities and "
-        f"wanted to connect regarding the {job_title.strip()} position {job_id_text.strip()} at {company.strip()}. Please take a look at my profile—"
+        f"wanted to connect regarding the {job_title} position {job_id_text} at {company}. Please take a look at my profile—"
         " I'd love to discuss how my skills could be a great fit. Thank you!"
     )
     return message
